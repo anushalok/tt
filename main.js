ea67f3a439fb27734a030fb004c1b77652029b0d
anushalok <125830572+anushalok@users.noreply.github.com>
2023-11-05 17:28:26 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+img = "";
+Status = "";
+objects = [];
+objectDetector = "";
+
+
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("Status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  Status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("Dog",45,75);
+    noFill();
+    stroke("#FF0000");
+    rect(30,60,450,350);
+
+    fill("#FF0000");
+    text("Cat",320,120);
+    nofill();
+    stroke("#FF0000");
+    rect(30,90,270,320);
+}
